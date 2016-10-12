927d4ea559f1180d9ebbc452aab89155af4e9557
johnneyChiu <johnney_chiu@163.com>
2016-10-12 14:29:16 +0800

    haha

--- account-persist/src/main/java/com/xiaoq/mvnbook/account/account_persist/AcccountPersistException.java
+++
@@ -1,9 +0,0 @@
-package com.xiaoq.mvnbook.account.account_persist;
-
-public class AcccountPersistException extends Exception {
-	private String msg;
-	public AcccountPersistException(String msg,Exception e){
-		System.out.println(msg);
-		e.printStackTrace();
-	}
-}
